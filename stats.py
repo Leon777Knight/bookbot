61043c4d0220df601edf338153dc1700d85dc3fe
Leon777Knight <leon777knight@gmail.com>
2025-11-15 23:27:35 +0000

    5: added function to create and order list of dictionaries

--- stats.py
+++ stats.py
@@ -11,3 +11,14 @@ def character_count(text):
         elif character in counter:
             counter[character] += 1
     return counter
+
+def order_count(diction):
+    return diction["count"]
+
+def listed_count(diction):
+    ordered_list = []
+    for character in diction:
+        ordered_list.append({"character": character, "count": diction[character]})
+    ordered_list.sort(reverse=True, key=order_count)
+    #print(f"List of dictionaries: {ordered_list}")
+    return ordered_list
