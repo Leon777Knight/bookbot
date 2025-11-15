9119b3b57e84ed95a0ec0842cc801bf1121122d3
Leon777Knight <leon777knight@gmail.com>
2025-11-15 23:32:51 +0000

    6: modified main.py to imporve presentation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import word_count
 from stats import character_count
+from stats import listed_count
 
 def get_book_text(filepath):
 
@@ -11,8 +12,17 @@ def get_book_text(filepath):
 def main():
     frankenstein = get_book_text("books/frankenstein.txt")
     num_of_words = word_count(frankenstein)
-    print(f"Found {num_of_words} total words")
+    #print(f"Found {num_of_words} total words")
     num_of_characters = character_count(frankenstein)
-    print(num_of_characters)
-
+    #print(num_of_characters)
+    ordered_count = listed_count(num_of_characters)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_of_words} total words")
+    print("--------- Character Count -------")
+    for item in ordered_count:
+        if item["character"].isalpha():
+            print(f"{item["character"]}: {item["count"]}")
+    print("============= END ===============")
 main()
